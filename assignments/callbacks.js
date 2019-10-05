d1584dcf5c2ff3539f93a6c3c2d8d8fba236d7e3
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2019-10-04 20:16:21 -0600

    Met mvp and did some stretch.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,32 +38,59 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
+  return cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
+getLength(items, console.log);
 
 function last(arr, cb) {
+  return cb(arr[arr.length - 1]);
   // last passes the last item of the array into the callback.
 }
+last(items, console.log);
 
 function sumNums(x, y, cb) {
+  let sum = x + y;
+  return cb(sum);
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+sumNums(1, 2, console.log);
 
 function multiplyNums(x, y, cb) {
+  let total = x * y;
+  return cb(total);
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+multiplyNums(5, 6, console.log);
 
+//--------------
 function contains(item, list, cb) {
+  const isInList = list.some(element => element == item);
+  // cb(list.some(element => element == item))
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(isInList);
 }
+contains("Penc", items, console.log);
 
 /* STRETCH PROBLEM */
+const nums = [1, 2, 3, 4, 5, 6, 6, 6];
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+  const table = {};
+  array.forEach(item => {
+    if (item in table) {
+      table[item] = table[item] + 1;
+    } else {
+      table[item] = 1;
+    }
+  });
+
+  cb(Object.keys(table));
 }
+
+removeDuplicates(nums, console.log);
+// removeDuplicates removes all duplicate values from the given array.
+// Pass the duplicate free array to the callback function.
+// Do not mutate the original array.
